cd3ad59eed21547d8ae07d27096d457a6d33c866
iqbalarighi <arighialfarisi@gmail.com>
2024-07-11 22:05:10 +0700

    update side

--- resources/views/layouts/side.blade.php
+++ resources/views/layouts/side.blade.php
@@ -154,7 +154,7 @@ if (notip.coun.count2 != 0){
     <body>
         {{-- user level 1 --}}
     @if ( Auth::user()->role === 'admin')
-        <div class="d-flex " id="wrapper">
+        <div class="d-flex {{ Request::is('dashboard') ? 'toggled' : ''}}" id="wrapper"> toggled
         <!-- Sidebar -->
         <div class="bg-light border-right " id="sidebar-wrapper">
         <div class="list-group list-group-flush sticky-top">
